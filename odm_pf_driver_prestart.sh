d7f81fe6c196adbd6e423617cd315f2c8a0f22ed
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2024-08-01 06:34:21 -0700

    odm_pf_driver: use the service script to bind the device
    Update the script to generate UUID on every reboot of the system
    The generated UUID will be updated in the odm_pf.cfg file
    
    Update the script to unbind the ODM PF device from kernel mode driver
    Binds the device to vfio.
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: Icd41c9f764f4a71c54bc1af1bd496790817331d5
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/odm_userspace_pf_driver/+/132563
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Tested-by: Jerin Jacob <jerinj@marvell.com>

---
+++ odm_pf_driver_prestart.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+FLAG_FILE="/var/run/uuid_generated"
+CFG_FILE="/etc/odm_pf_driver.cfg"
+PCI_DEVICE="0000:08:00.0"
+DRIVER="vfio-pci"
+UUID_LINE="UUID="
+
+# Check if the UUID has already been generated
+if [ ! -f "$FLAG_FILE" ]; then
+    # Generate a new UUID
+    NEW_UUID=$(uuidgen)
+    echo "Generated UUID: $NEW_UUID"
+
+    # Update the target file with the new UUID
+    if grep -q "^$UUID_LINE" "$CFG_FILE"; then
+        # Replace the existing UUID line
+        sed -i "s/^$UUID_LINE.*/$UUID_LINE$NEW_UUID/" "$CFG_FILE"
+    else
+        # Append the UUID line if it doesn't exist
+        echo "$UUID_LINE$NEW_UUID" >> "$CFG_FILE"
+    fi
+
+    # Create the flag file to indicate the UUID has been generated
+    touch "$FLAG_FILE"
+fi
+
+echo "Unbinding PCI device $PCI_DEVICE"
+echo $PCI_DEVICE > /sys/bus/pci/devices/$PCI_DEVICE/driver/unbind
+echo "Binding PCI device $PCI_DEVICE to driver $DRIVER"
+echo "177d a08b" > /sys/bus/pci/drivers/vfio-pci/new_id
